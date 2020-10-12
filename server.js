2413094b313e39cac7081c71c87a8f495dcc3ceb
Jani Muhlestein <janimuhlestein@gmail.com>
2020-10-12 04:34:13 -0600

    left join

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ app.use(express.json());
 
 //get all candidates
 app.get('/api/candidates', (req,res)=> {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
     const params = [];
 
 db.all(sql, params, (err, rows) => {
@@ -36,8 +40,12 @@ db.all(sql, params, (err, rows) => {
 
 //get one candidate
 app.get('/api/candidate/:id', (req,res) => {
-    const sql = `SELECT * FROM candidates
-        WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err,row) => {
         if(err) {
